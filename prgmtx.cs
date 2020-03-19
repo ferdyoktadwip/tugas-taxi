4c5d9a12600762fdab2c090783ec50329c0bf0df
ferdyoktadwip <ferdy.1a998@students.amikom.ac.id>
2020-03-19 19:41:20 +0700

    tugas

---
+++ prgmtx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace LatihanClass
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// membuat objek dari class taxi
+
+			Taxi taxi = new Taxi();
+			
+
+			// mengeset nilai properties objek saya dan kamu
+			taxi.DriverName = "FerdyOkta";
+			taxi.OnDuty = true;
+			taxi.NumPassenger = 10;
+
+			// memanggil method registrasi dan isi krs
+			taxi.TaxiInfo();
+			taxi.PickupPassenger();
+			taxi.DropOffPassenger();
+
+			Console.ReadKey();
+		}
+
+	}
+}
---
+++ taxi.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace LatihanClass
+{
+	public class Taxi
+	{
+		// Properties
+		public string Driver { get; set; }
+		public bool OnDuty = true;
+		public int NumPassenger { get; set; }
+
+		// method
+		public void TaxiInfo()
+		{
+			Console.WriteLine("Driver Name: {0}", Driver);
+			Console.Write("On Duty: ");
+			Console.WriteLine(OnDuty ? "Yes" : "No");
+			Console.WriteLine("Num of Passenger: {0}", NumPassenger);
+			
+		}
+
+		public void PickupPassenger()
+		{
+			Console.WriteLine("{0} sedang menjemput ", Driver);
+		}
+
+		public void DropOffPassenger()
+		{
+			Console.WriteLine("{0} selesai mengantar  ", Driver);
+		}
+
+	}
+}
